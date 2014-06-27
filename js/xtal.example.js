fd1754ea79f60b31339518a01605de453ba7546a
Ian Rees <ian@ianrees.net>
2014-06-26 17:03:12 -0700

    xtal.js module stub

---
+++ js/xtal.example.js
@@ -0,0 +1,23 @@
+/* 
+
+xtal.js Module Example
+
+Exports:
+	Hello
+
+*/
+var xtal = (function(module) {return module})(xtal||{});
+xtal.example = (function(module) {
+	
+	function Hello() {
+		this.test = 123;
+	}
+	something.prototype.ok = function(hello) {
+		return "world"
+	}
+	
+  // Exports
+  return {
+    'Hello': Hello
+  }
+})(xtal);
